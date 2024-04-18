1f009e441b182da111ae43705afdf836e8b5f23a
Jason Miracle Gunawan <jasonmiracle15@gmail.com>
2024-04-18 12:01:28 +0700

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by student on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
