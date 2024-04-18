7f26587c457bb4fc6994e0e082f4d22f8e0a3168
Jason Miracle Gunawan <jasonmiracle15@gmail.com>
2024-04-18 13:14:21 +0700

    Chapter 1 - Create the List of Landmarks

---
+++ Landmark/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmark
+//
+//  Created by student on 18/04/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
