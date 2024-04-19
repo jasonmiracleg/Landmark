68968e0f13046141305dde14ac853691c5d1e2b0
Jason Miracle Gunawan <jasonmiracle15@gmail.com>
2024-04-19 07:58:31 +0700

    Chapter 1 -  Pass Data into Child Views

--- Landmark/Views/LandmarkDetail.swift
+++ Landmark/Views/LandmarkDetail.swift
@@ -8,41 +8,46 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
+        ScrollView {
+            VStack {
+                MapView(coordinate: landmark.locationCoordinate)
+                    .frame(height: 300)
+                
+                CircleImage(image: landmark.image)
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                
+                VStack(alignment: .leading) {
+                    Text(landmark.name)
+                        .font(.title)
+                    HStack {
+                        Text(landmark.park)
+                            .font(.subheadline)
+                        Spacer()
+                        Text(landmark.state)
+                            .font(.subheadline)
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                    
+                    Divider()
+                    
+                    Text("About \(landmark.name)")
+                        .font(.title2)
+                    Text(landmark.description)
                 }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
                 
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive Text Goes Here")
+                .padding()
             }
-            
-            .padding()
-            Spacer()
+            .navigationTitle(landmark.name)
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
 
 #Preview {
-    LandmarkDetail()
+    LandmarkDetail(landmark: landmarks[0])
 }
