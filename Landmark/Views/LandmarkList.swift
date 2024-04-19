661824c3997a591e863bad6f6a1d831d0f111ab2
Jason Miracle Gunawan <jasonmiracle15@gmail.com>
2024-04-19 08:18:44 +0700

    Chapter 1 -  Add a Control to Toggle the State

--- Landmark/Views/LandmarkList.swift
+++ Landmark/Views/LandmarkList.swift
@@ -19,13 +19,19 @@ struct LandmarkList: View {
     var body: some View {
         /*id: \.id use this if there's no identifiable protocol in list parameter*/
         NavigationSplitView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List{
+                Toggle(isOn: $showFavoritesOnly){
+                    Text("Favorites Only")
+                }
+                ForEach (filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
         } detail: {
             Text("Select a Landmark")
