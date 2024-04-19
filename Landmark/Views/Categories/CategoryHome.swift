74e5d18b1e9881e6b768846a175b23657332605e
Jason Miracle Gunawan <jasonmiracle15@gmail.com>
2024-04-19 10:29:48 +0700

    Chapter 3 -  Add a Category View

---
+++ Landmark/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmark
+//
+//  Created by student on 19/04/24.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationSplitView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
