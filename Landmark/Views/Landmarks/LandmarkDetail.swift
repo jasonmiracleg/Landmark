e59ad998de9fd03a9faa499b93578eaf014644ed
Jason Miracle Gunawan <jasonmiracle15@gmail.com>
2024-04-19 09:02:56 +0700

    Chapter 1 -  Create a Favorite Button for each Landmark

--- Landmark/Views/Landmarks/LandmarkDetail.swift
+++ Landmark/Views/Landmarks/LandmarkDetail.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
     
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
+        @Bindable var modelData = modelData
+        
         ScrollView {
             VStack {
                 MapView(coordinate: landmark.locationCoordinate)
@@ -21,14 +28,15 @@ struct LandmarkDetail: View {
                     .padding(.bottom, -130)
                 
                 VStack(alignment: .leading) {
-                    Text(landmark.name)
-                        .font(.title)
+                    HStack {
+                        Text(landmark.name)
+                            .font(.title)
+                        FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                    }
                     HStack {
                         Text(landmark.park)
-                            .font(.subheadline)
                         Spacer()
                         Text(landmark.state)
-                            .font(.subheadline)
                     }
                     .font(.subheadline)
                     .foregroundStyle(.secondary)
@@ -39,7 +47,6 @@ struct LandmarkDetail: View {
                         .font(.title2)
                     Text(landmark.description)
                 }
-                
                 .padding()
             }
             .navigationTitle(landmark.name)
@@ -49,5 +56,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
 }
