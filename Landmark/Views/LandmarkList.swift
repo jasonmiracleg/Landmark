a86c4371434fdfdbeff19fc17d2b63dd30796638
Jason Miracle Gunawan <jasonmiracle15@gmail.com>
2024-04-19 08:14:29 +0700

    Chapter 1 - Filter the List View

--- Landmark/Views/LandmarkList.swift
+++ Landmark/Views/LandmarkList.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         /*id: \.id use this if there's no identifiable protocol in list parameter*/
         NavigationSplitView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
