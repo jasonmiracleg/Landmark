60518d9124775d9e6f5df13a9b48fbf24f7116f5
Jason Miracle Gunawan <jasonmiracle15@gmail.com>
2024-04-25 11:57:20 +0700

    Chapter 3 - Display a User Profile

---
+++ Landmark/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmark
+//
+//  Created by student on 25/04/24.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotification = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username : "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "☀️"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { rawValue }
+    }
+}
---
+++ Landmark/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmark
+//
+//  Created by student on 25/04/24.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
